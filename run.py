f402ee9b99b7dc82d789ddaf0caa50ed6d53aca6
zews282-cell <zews282@gmail.com>
2026-09-06 11:45:42 +0300

    Add main entry point to run.py for Uvicorn

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", "8000"))
+
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=port,
+    )
